916cba27a72feae524620a231461db04a13887a1
Aravind <aravindm3095@gmail.com>
2017-02-27 21:48:20 -0500

    Express Added

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Corridors - Node + Express Setup');
+})
+
+var server = app.listen(3000, function () {
+   var host = server.address().address
+   var port = server.address().port
+   
+   console.log("Example app listening at http://localhost:%s", port)
+})
