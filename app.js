661fea83dcdefd8b2ad36219f601042502def1c0
MatiMaster <mai261@nyu.edu>
2017-03-26 18:32:27 -0400

    theBasics

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-const express = require('express');
-const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-   res.send('corridor - Node + Express Setup');
+var SERVER_PORT = 3000;
+var LOG_CONNECTIONS_CONSOLE = true;
+
+var app = require('express')();
+var http = require('http').createServer(app);
+
+
+
+function getHomePage(request,response){
+    if(LOG_CONNECTIONS_CONSOLE){
+        console.log('Serving::Person has connected and requested home page');
+    }
+    response.sendFile(__dirname + '/Templates/login.html');
+}
+
+
+app.get('/', getHomePage);
+
+app.get( '/*' , function( req, res, next ) {
+    //This is the current file they have requested
+    var file = req.params[0];
+    //For debugging, we can track what files are requested.
+    if(LOG_CONNECTIONS_CONSOLE) console.log('\t :: Express :: file requested : ' + file);
+    //Send the requesting client the file.
+    res.sendFile( __dirname + '/' + file );
+
 });
 
-app.listen(port, (err) => {
-   if(err){
-   	return console.log("Alright you got shafted by ", err);
-   }
-   console.log("corridor is up and running at http://localhost:%s", port);
+
+
+http.listen(process.env.PORT|| SERVER_PORT, function(){
+    console.log('Listening on port ' + SERVER_PORT);
+    console.log(http.address());
 });
+
+
